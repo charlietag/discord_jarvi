c2d183925dd2107a86d7bba098a63fa5bbba2022
Charlie Hsieh <charlieofficial@gmail.com>
2022-02-21 23:41:37 +0800

    add skip commands into message event - regex

--- apps/message.rb
+++ apps/message.rb
@@ -44,30 +44,31 @@ bot = Discordrb::Bot.new token: $bot_token
 # same as
 # bot.message(with_text: /[^#{newbie_fail}]*(#{newbie_ans})+[^#{newbie_fail}]*/i) do |event|
 
-bot.message(with_text: /(?:(?!#{newbie_fail}).)*(#{newbie_ans})+(?:(?!#{newbie_fail}).)*/i) do |event|
-  p event.message.content
-  p "^\/(#{$skip_commands.join('|')})"
-  p event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
+bot.message(with_text: /(?:(?!^\/(#{$skip_commands.join('|')})))(?:(?!#{newbie_fail}).)*(#{newbie_ans})+(?:(?!#{newbie_fail}).)*/i) do |event|
 
-  if not event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
-    if $activated_servers.include? event.server.name
-      if $message_channels.include? event.channel.name
+  # --- debug ---
+  # p event.message.content
+  # p "^\/(#{$skip_commands.join('|')})"
+  # p event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
+  # --- debug ---
 
-        # ---------------------------------------------
-        # show only when correct answer is not been chosen
-        # ---------------------------------------------
-        if newbie_answered != 'y'
-          event.respond newbie_ans_msg
+  if $activated_servers.include? event.server.name
+    if $message_channels.include? event.channel.name
+
+      # ---------------------------------------------
+      # show only when correct answer is not been chosen
+      # ---------------------------------------------
+      if newbie_answered != 'y'
+        event.respond newbie_ans_msg
 
 
-          # --- mark as answered, and reload the variable ---
-          script.lock
-          File.open($newbie_answered,"w")  {  |f|  f.write  "y"  }
-          newbie_answered = File.read($newbie_answered)
-          script.unlock
-        end
-
+        # --- mark as answered, and reload the variable ---
+        script.lock
+        File.open($newbie_answered,"w")  {  |f|  f.write  "y"  }
+        newbie_answered = File.read($newbie_answered)
+        script.unlock
       end
+
     end
   end
 end
@@ -76,19 +77,23 @@ end
 # correct
 # ------------
 # bot.message(with_text: /(.*(#{newbie_fail})+.*){2,}/i) do |event|
-bot.message(with_text: /(.*(#{newbie_fail})+.*){1,}/i) do |event|
-  p event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
-  if not event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
-    if $activated_servers.include? event.server.name
-      if $message_channels.include? event.channel.name
-        # ---------------------------------------------
-        # show only when correct answer is not been chosen
-        # ---------------------------------------------
-        if newbie_answered != 'y'
-          event.respond newbie_fail_msg
-        end
+bot.message(with_text: /(?:(?!^\/(#{$skip_commands.join('|')})))(.*(#{newbie_fail})+.*){1,}/i) do |event|
 
+  # --- debug ---
+  # p event.message.content
+  # p "^\/(#{$skip_commands.join('|')})"
+  # p event.message.content.match?(/^\/(#{$skip_commands.join('|')})/)
+  # --- debug ---
+
+  if $activated_servers.include? event.server.name
+    if $message_channels.include? event.channel.name
+      # ---------------------------------------------
+      # show only when correct answer is not been chosen
+      # ---------------------------------------------
+      if newbie_answered != 'y'
+        event.respond newbie_fail_msg
       end
+
     end
   end
 end
